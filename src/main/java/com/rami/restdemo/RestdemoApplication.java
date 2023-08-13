dd2d097c459d7b750b1ffa0a107c7bfc0ae475f8
ramiradaideh <you@example.com>
2023-08-13 10:19:43 -0400

    Initial commit

---
+++ src/main/java/com/rami/restdemo/RestdemoApplication.java
@@ -0,0 +1,14 @@
+package com.rami.restdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestdemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("GFG" + "sd" + "gfg");
+		SpringApplication.run(RestdemoApplication.class, args);
+	}
+
+}
